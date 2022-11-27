bad6d1de27445a0be422375a9292916065f7b60d
Iflar <classicsience@gmail.com>
2022-11-27 18:49:10 -0500

    added base movie service class

---
+++ src/app/movie.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { Movie } from './movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() { }
+}
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movies',
