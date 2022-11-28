357b40034375245ba96b4d369f3a00f8b441e57a
Iflar <classicsience@gmail.com>
2022-11-27 19:04:36 -0500

    added 'angular-in-memory-web-api' for server simulation + created in-memory-data.service to simulate a database

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Movie } from './movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+
+  constructor() { }
+}
